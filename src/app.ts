9d0305488b7225cffa4a253ab239a1783f1f2034
Fábio Júnior <fjrleao@gmail.com>
2023-07-18 16:36:44 -0300

    docs: create route documentation

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import 'express-async-errors'
 import express, { Application, Request, Response } from 'express'
-import { AppError, handleError } from './errors'
-import mongoose from 'mongoose'
+import { handleError } from './errors'
 import beerRoutes from './routes/beer.routes'
+import path from 'path'
 
 const app: Application = express()
 app.use(express.json())
 
 app.use('/beers', beerRoutes)
+
+app.get('/docs', (req: Request, res: Response) => {
+	res.sendFile(path.join(__dirname, './docs/index.html'))
+})
 
 app.use(handleError)
 export default app
