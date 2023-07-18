d9ccc4e68eed273d80b7ecc77729d3a6314653cc
Fábio Júnior <fjrleao@gmail.com>
2023-07-18 20:12:06 -0300

    remove all files

--- src/services/deleteBeer.service.ts
+++
@@ -1,7 +0,0 @@
-import { Beer } from '../models/Beer'
-
-const deleteBeerService = async (id: string) => {
-	await Beer.deleteOne({ _id: id }).exec()
-}
-
-export default deleteBeerService
--- src/services/listBeer.service.ts
+++
@@ -1,8 +0,0 @@
-import { Beer } from '../models/Beer'
-
-const listBeerService = async () => {
-	const beers = await Beer.find().exec()
-	return beers
-}
-
-export default listBeerService
